11919b99de75760f64141a9f68ed2f8110939fb9
MtzFranVictorManuel <40128870+MtzFranVictorManuel@users.noreply.github.com>
2020-06-16 20:08:59 -0500

    creacion de boton "login"
    se ingresaron los metodos para verificar que el estudainte y el maestro existen en la base de datos.

--- src/appgestorpracticasprofecionales/FXMLindexNewController.java
+++ src/appgestorpracticasprofecionales/FXMLindexNewController.java
@@ -12,6 +12,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import datos.Conexion;
+import datos.StudentConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -19,9 +24,103 @@ import datos.Conexion;
  */
 public class FXMLindexNewController implements Initializable {
     
+    
+    private static final String SQL_CONSULTAESTUDIANTE = "SELECT * FROM tbl_estudiante WHERE tbl_estudiante.matricula=? AND  tbl_estudiante.password=?";
+    private static final String SQL_CONSULTAPROFESOR = "SELECT * FROM tbl_profesor WHERE tbl_profesor.nombre=? AND  tbl_profesor.password=?";
+    
     @FXML
     private TextField textNameUser;
+    @FXML
     private PasswordField textPassword;
+    
+    @FXML
+    private void initializeLogin(ActionEvent event){
+        String userName = textNameUser.getText();
+        String password = textPassword.getText();
+
+        if(!userName.isEmpty() && !password.isEmpty()){
+            if(userName.startsWith("xd"))
+            {
+                consultarEstudiante(userName, password);
+            }else{
+                consultarProfresor(userName, password);
+            }
+        }
+        else{
+        Alert alertEmptyInfo = new Alert(Alert.AlertType.ERROR);
+        alertEmptyInfo.setTitle("Error");
+        alertEmptyInfo.setHeaderText(null);
+        alertEmptyInfo.setContentText("Los campos de usauario y/o contraseña no han sido ingresados");
+        alertEmptyInfo.showAndWait();
+        
+        }
+    }
+    
+    private void consultarEstudiante(String matricula, String password){
+        Connection connect = Conexion.getConexion();
+        PreparedStatement stmt = null;
+        if(connect != null){
+            try{
+                stmt = connect.prepareStatement(SQL_CONSULTAESTUDIANTE);
+                stmt.setString(1, matricula);
+                stmt.setString(2, password);
+                ResultSet rs = stmt.executeQuery();
+                if(rs.next()){
+                    String nomUser = rs.getString("nombre");
+                    String apellidoPaternoUser = rs.getString("apellidoPaterno");
+                    
+                    Alert alertExitoso = new Alert(Alert.AlertType.INFORMATION);
+                    alertExitoso.setTitle("Acceso correcto");
+                    alertExitoso.setHeaderText(null);
+                    alertExitoso.setContentText("Bienvenido " + nomUser + " " + apellidoPaternoUser + " al sistema");
+                    alertExitoso.showAndWait();
+                }
+                else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Usauario y/o contraseña incorrecto...");
+                    alert.showAndWait();
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void consultarProfresor(String nombre, String password){
+        Connection connect = Conexion.getConexion();
+        PreparedStatement stmt = null;
+        if(connect != null){
+            try{
+                stmt = connect.prepareStatement(SQL_CONSULTAPROFESOR);
+                stmt.setString(1, nombre);
+                stmt.setString(2, password);
+                ResultSet rs = stmt.executeQuery();
+                if(rs.next()){
+                    String nomUser = rs.getString("nombre");
+                    String apellidoPaternoUser = rs.getString("apellidoPaterno");
+                    
+                    Alert alertExitoso = new Alert(Alert.AlertType.INFORMATION);
+                    alertExitoso.setTitle("Acceso correcto");
+                    alertExitoso.setHeaderText(null);
+                    alertExitoso.setContentText("Bienvenido " + nomUser + " " + apellidoPaternoUser + " al sistema");
+                    alertExitoso.showAndWait();
+                }
+                else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Usauario y/o contraseña incorrecto...");
+                    alert.showAndWait();
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
